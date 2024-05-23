5b8bf6c68c337972326fa7f8b02e61b3409e139d
Livia Nicoly <livianicck@gmail.com>
2024-05-23 10:23:46 -0300

    header completa

---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <title>Brand Persona | Sua Persona Virtual com alta performance</title>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php wp_head(); ?>
+
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/nautilus/assets/css/stylesheet.css" />
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    
+    <header id="header">
+        <!-- Header here -->
+        <div class="header">
+            <div class="container">
+                <div class="flex">
+                    <img src="/wp-content/themes/nautilus/assets/img/brand-persona-logo-4x.png" class="logoBrand">
+                
+                    <nav>    
+                         <ul>
+                            <li><a href="#home">HOME</a></li>
+                            <li><a href="#pq-brandpersona">POR QUE BRAND PERSONA</a></li>
+                            <li><a href="#o-mercado">O MERCADO</a></li>
+                            <li><a href="#vantagens">VANTAGENS</a></li>
+                            <li><a href="#como-funciona">COMO FUNCIONA</a></li>
+                            <li><a href="#o-combo">O COMBO</a></li>
+                            <li><a href="#contato">CONTATO</a></li>
+                         </ul>
+                            
+                    </nav>
+                
+               
+             
+                </div>    
+            
+            </div>
+        </div>    
+    
+    </header>
+
+    <section class="home" id="home">
+            <div class="personagem1">
+                <img src="/wp-content/themes/nautilus/assets/img/secao-01-personagem.png" alt="">
+            </div/>
+
+            <div class="containerbx">
+                    <div class="box">
+                        <span>leve sua empresa para a nova realidade digital </span>
+                        <h1>A sua persona virtual com alta performance</h1>
+                        <p>Para marcas inovadoras: Brand Persona PYXYS + Ilustraria  é o primeiro combo do Brasil
+                         de construção de avatares 3D, com tecnologia de ponta em computação gráfica, combinada 
+                         à mais completa Estratégia de  Content, Growth e Performance Data-driven</p>
+                        <button type="button" class="btn btn-outline-light">QUERO SABER MAIS</button>
+                        <div class="rod">
+                            <span>Um produto:</span>
+                            <img src="/wp-content/themes/nautilus/assets/img/pyxys-logo-4x.png" class="logopx">
+                            <img src="/wp-content/themes/nautilus/assets/img/logoill.png" class="logoill">
+                        </div>
+                    </div>                    
+            </div>
+    </section>
+
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+
+<!-- Home page content here -->
+
+    
+<?php get_footer(); ?>
